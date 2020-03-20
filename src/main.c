bc4bb0edb99b5680085f6b3b8fa5a861746a7c09
suwacrab <suwacrab@tilde.town>
2020-03-20 10:25:07 +0000

    create!

--- src/main.c
+++ src/main.c
@@ -13,11 +13,14 @@ typedef struct gmem
 
 /*	--	vars	--	*/
 u8 fcmem[ MBSIZE(4) ];
-gmem *gram = (gmem*)fcmem;
+gmem *gram = NULL;
 
 int main(void)
 {
+	// init
+	gram = (gmem*)fcmem;
 	kanako *suwa_pool = kanako_init(&gram->suwa_pool,gram->objlist,0x400);
+	// main
 	for(u32 i=0; i<4; i++) printf("objs[%d]: %p\n",i,&suwa_pool->objs[i]);
 }
 
